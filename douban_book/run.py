20b5005dd462824cfd959b1ef7b3c282ba390b4b
alexchung <yonganzhong@outlook.com>
2020-06-19 17:20:29 +0800

    add crawlprocess method

--- douban_book/run.py
+++ douban_book/run.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 #------------------------------------------------------
 # @ File       : run.py
-# @ Description:  
+# @ Description:
+# @ Reference: reference https://docs.scrapy.org/en/latest/topics/practices.html
 # @ Author     : Alex Chung
 # @ Contact    : yonganzhong@outlook.com
 # @ License    : Copyright (c) 2017-2018
@@ -14,11 +15,30 @@ import os
 import sys
 from scrapy.commands.crawl import Command
 from scrapy.cmdline import execute
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+from scrapy.crawler import CrawlerRunner
+from twisted.internet import reactor
+from scrapy.utils.log import configure_logging
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 if __name__ == "__main__":
+
     # Command().run(args=["scrapy crawl itcast"], opts="-o teacher.csv")
 
+    # method 1 execute
     # execute(argv=["scrapy", "crawl", "douban_book"])
-    execute(argv=["scrapy", "crawl", "douban_book"])
+
+    # method 2 CrawlerProcess（recommend）
+    process = CrawlerProcess(get_project_settings())
+    process.crawl("douban_book", domain="douban.com")
+    process.start()
+
+    # method 3 CrawlerRunner
+    # configure_logging()
+    # runner = CrawlerRunner(get_project_settings())
+    # runner.crawl("douban_book")
+    # d = runner.join()
+    # d.addBoth(lambda _: reactor.stop())
+    # reactor.run()
